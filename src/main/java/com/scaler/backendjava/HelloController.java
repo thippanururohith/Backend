62027e50c9256b890d37dd505839bebc988ca08d
rohit <rohith.reddy.thippanuru@gmail.com>
2025-03-04 13:38:49 -0600

    my first api

--- src/main/java/com/scaler/backendjava/HelloController.java
+++ src/main/java/com/scaler/backendjava/HelloController.java
@@ -1,5 +1,6 @@
 package com.scaler.backendjava;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,16 @@ public class HelloController {
         return "Hello ,i am Rohith";
 
     }
+    @RequestMapping(value = "/hello/{id}", method = RequestMethod.GET)
+    public String sayHello(@PathVariable("id") String id) {
+        return "Hello ,i am" + id ;
+
+    }
 }
 
+
+//RestController to tell java that there are some apis in the code
+//RequestMapping to make method an api
+//Requestmethod.get read
+//pathvariable to assign value to the variable
+// from fontend if you get some value to assign it to a variable
